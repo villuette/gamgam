7ce3da00a8efd2480d5271be19dfee8be39df9ca
LayqBlack <azat.akhmetzyanov2003@gmail.com>
2022-03-20 23:45:20 +0500

    Добавил пиксельный шрифт

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Stats
+{
+    public int GG_Experience = 0;
+    public int GG_Gold = 0;
+    public int GG_Health = 300;
+    public int GG_Mana = 0;
+    public int GG_Damage = 6;
+    public float GG_Armor = 0.0f;
+    public float GG_CRT_CHN = 0.1f;
+    public float GG_CRT_DMG = 1.0f;
+    public float GG_SUP = 0.0f;
+    public int GG_SUP_Manacost = 1;
+}
