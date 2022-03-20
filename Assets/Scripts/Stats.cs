6b473ab7e1cb5eabf1945ba8cf6590b0906744a6
LayqBlack <azat.akhmetzyanov2003@gmail.com>
2022-03-21 00:29:03 +0500

    a

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -1,10 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Stats
+public interface Stats
 {
-    public int GG_Experience = 0;
-    public int GG_Gold = 0;
-    public int GG_Health = 300;
+    public int GG_Experience { get => GG_Experience; set => GG_Experience = value; };
+    public int GG_Gold;
+    public int GG_Health;
     public int GG_Mana = 0;
     public int GG_Damage = 6;
     public float GG_Armor = 0.0f;
