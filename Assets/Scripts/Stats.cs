2be6320647efee5cdd690711c606bd0370813f5d
LayqBlack <azat.akhmetzyanov2003@gmail.com>
2022-03-22 00:41:52 +0500

    Добавил алгоримт бехавиоур

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface Stats
+public class Stats
 {
-    public int GG_Experience { get => GG_Experience; set => GG_Experience = value; };
+    public int GG_Experience;
     public int GG_Gold;
     public int GG_Health;
-    public int GG_Mana = 0;
-    public int GG_Damage = 6;
-    public float GG_Armor = 0.0f;
-    public float GG_CRT_CHN = 0.1f;
-    public float GG_CRT_DMG = 1.0f;
-    public float GG_SUP = 0.0f;
-    public int GG_SUP_Manacost = 1;
+    public int GG_Mana;
+    public int GG_Damage;
+    public float GG_Armor;
+    public float GG_CRT_CHN;
+    public float GG_CRT_DMG;
+    public float GG_SUP_DMG;
+    public int GG_SUP_Manacost;
+
+
 }
