794172d15f1231c9e7131711e9db9b4dbe216089
villuette <maksim.yarusov15092003@gmail.com>
2022-03-26 03:46:45 +0500

    подключил анимацию к персонажу

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class Stats
 {
-    public int GG_Experience;
+    protected int GG_Experience = 0;
     public int GG_Gold;
     public int GG_Health;
-    public int GG_Mana;
-    public int GG_Damage;
-    public float GG_Armor;
-    public float GG_CRT_CHN;
-    public float GG_CRT_DMG;
-    public float GG_SUP_DMG;
-    public int GG_SUP_Manacost;
-
+    public int GG_Mana = 0;
+    public int GG_Damage = 6;
+    public float GG_Armor = 0.0f;
+    public float GG_CRT_CHN = 0.1f;
+    public float GG_CRT_DMG = 1.0f;
+    public float GG_SUP = 0.0f;
+    public int GG_SUP_Manacost = 1;
 
 }
