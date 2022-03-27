9f7492008720aa820da50b153cc9c354174f0e10
villuette <maksim.yarusov15092003@gmail.com>
2022-03-27 05:12:29 +0500

    Сделал переход анимаций

--- Assets/Scripts/GG_Moving.cs
+++ Assets/Scripts/GG_Moving.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GG_Moving : MonoBehaviour
 {
     Rigidbody2D rb;
     public float speed;
+    Animator anim;
+    public float x;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         transform.position = gameObject.transform.position;
     }
     void FixedUpdate()
     {
+        
         Flip();
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
 
+    }
+    private void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        if (Math.Abs(Input.GetAxis("Horizontal")) > 0.1f)
+            anim.SetInteger("is_running", 1);
+        else
+            anim.SetInteger("is_running", 0);
     }
     void Flip()
     {
