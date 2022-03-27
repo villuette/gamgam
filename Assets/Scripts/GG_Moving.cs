db138d2c8d5fb807122c7dc477abff7a28b6b140
Denis <muharamov14@gmail.com>
2022-03-27 20:16:48 +0500

    тест

--- Assets/Scripts/GG_Moving.cs
+++ Assets/Scripts/GG_Moving.cs
@@ -9,16 +9,17 @@ public class GG_Moving : MonoBehaviour
     public float speed;
     Animator anim;
     public float x;
-    
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         transform.position = gameObject.transform.position;
     }
+
     void FixedUpdate()
     {
-        
+        //CheckingLadder();
         Flip();
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
         if (rb.velocity.x != 0)
@@ -33,5 +34,36 @@ public class GG_Moving : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         if (Input.GetAxis("Horizontal") < 0)
             transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    public float check_RADIUS = 0.04f;
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(CHECK_ladder.position, check_RADIUS);
+    }
+    public Transform CHECK_ladder;
+    public bool checkedLadder;
+    public LayerMask LadderMask;
+    //void CheckingLadder()
+    //{
+       // checkedLadder = Physics2D.OverlapPoint(CHECK_ladder.position, LadderMask);
+   // }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger!");
+        if (collision.gameObject.tag == "ladder")
+        {
+            checkedLadder = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "ladder")
+        {
+            checkedLadder = false;
+        }
     }
 }
